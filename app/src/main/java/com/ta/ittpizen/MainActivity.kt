3609ff7a678a52ce7aa192599dd026f443229fee
womenincode <amitaputri55@gmail.com>
2023-12-24 20:09:30 +0700

    feature(ui-component): Create ui component

--- app/src/main/java/com/ta/ittpizen/MainActivity.kt
+++ app/src/main/java/com/ta/ittpizen/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.ta.ittpizen.domain.entity.PostItem
 import com.ta.ittpizen.feature_auth.di.authModule
 import com.ta.ittpizen.navigation.ITTPizenNavHost
+import com.ta.ittpizen.ui.component.post.PostItem
 import com.ta.ittpizen.ui.theme.ITTPizenTheme
 import org.koin.compose.KoinApplication
 
@@ -29,7 +33,24 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.fillMaxSize(),
                         color = MaterialTheme.colorScheme.background
                     ) {
-                        ITTPizenNavHost()
+//                        ITTPizenNavHost()
+                        val postItem = PostItem(
+                            name = "Amita Putry Prasasti",
+                            type = "Student",
+                            date = "1 hours ago",
+                            profile = "",
+                            text = "Haloo, salam kenal, mari saling koneksi temen-temen. Haloo, salam kenal, mari saling koneksi temen-temen. Haloo, salam kenal, mari saling koneksi temen-temen. Haloo, salam kenal, mari saling koneksi temen-temen",
+                            media = "https://kemahasiswaan.ittelkom-pwt.ac.id/wp-content/uploads/sites/27/2021/05/pilmapres20211-1200x675.png",
+                            liked = true
+                        )
+                        PostItem(
+                            post = postItem,
+                            modifier = Modifier.padding(
+                                start = 20.dp,
+                                top = 20.dp,
+                                end = 20.dp
+                            )
+                        )
                     }
                 }
             }
